af39721c5c456bbdb244d6d58d4aefbc088d578d
Midcoast Solidarity <midcoastsolidarity@protonmail.com>
2026-02-13 15:32:05 -0500

    Remove JS files

--- assets/js/site-protection.js
+++
@@ -1,4 +0,0 @@
-// prevent framing (frame-bust)
-try {
-  if (window.top !== window.self) window.top.location.replace(window.self.location.href);
-} catch (e) { /* ignore cross-origin frame errors */ }
--- assets/js/year.js
+++
@@ -1,5 +0,0 @@
-// populate copyright year
-try {
-  const el = document.getElementById('year');
-  if (el) el.textContent = new Date().getFullYear();
-} catch (e) { console.error(e); }
